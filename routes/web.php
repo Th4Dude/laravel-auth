aabd033789a6c4c11fa0a1509c4e62a78bc8d94b
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-17 17:34:51 +0300

    fix dashboard view

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/', function() {
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+    /* Route::resource('posts', ProjectController::class ); */
+
+
+
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
